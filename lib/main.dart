cdfb9dee6bba2d6baa5d458e7d652b06c87e1511
Juan Ayala <juandanielayalaquinones@gmail.com>
2026-03-07 23:51:04 -0500

    the third  part of my mini Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_auth_provider/auth/auth_gate.dart';
 import 'package:supabase_auth_provider/providers/auth_provider.dart';
@@ -7,9 +8,12 @@ import 'package:supabase_auth_provider/providers/note_provider_stream.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
-  final supabaseUrl = 'https://kdjnhtcgaiqnjlyvlqjb.supabase.co';
-  final supabaseKey = 'sb_publishable_pTVL8x3ToteT8KbkM-1xpg_XiGbXeTe';
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env"); 
+  final supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  final supabaseKey = dotenv.env['SUPABASE_KEY']!;
+
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
 
   runApp(const MyApp());
@@ -24,9 +28,11 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
-         ChangeNotifierProvider<NoteProvider>(create: (_) => NoteProvider()),
+        ChangeNotifierProvider<NoteProvider>(create: (_) => NoteProvider()),
 
-         ChangeNotifierProvider<NoteProviderStream>(create: (_) => NoteProviderStream()),
+        ChangeNotifierProvider<NoteProviderStream>(
+          create: (_) => NoteProviderStream(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
